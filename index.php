216a8951f50f5f295993491f483bca75ade21075
Ricky944902 <ricky_xs03@163.com>
2025-05-16 19:58:31 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,2 @@
 <?php
-// There is nothing output here because block themes do not use php templates.
-// There is a core ticket discussing removing this requirement for block themes:
-// https://core.trac.wordpress.org/ticket/54272.
+// Silence is golden.
